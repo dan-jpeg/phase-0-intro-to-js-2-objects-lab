a864572a2195b2e49fe4dcc3c694b992ab2a8e0e
Dan Crowley <dancr.wley@gmail.com>
2023-01-20 17:46:18 -0600

    objet lab complet

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {
+    
+    name: "riicardio m.",
+    streetAddress: "666 real street, anytown, usa"
+
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    
+    return {
+        ...employee,
+        [key]: value,
+    };
+
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   
+    employee[key] = value;
+        return employee;
+
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    
+   employee = { ...employee };
+
+    delete employee[key];
+
+    return employee;
+
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
