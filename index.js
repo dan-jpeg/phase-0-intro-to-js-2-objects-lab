f56ba5109eb2475d4d0e590d3e2a172662f36d6a
dan <dancr.wley@gmail.com>
2023-04-11 10:41:10 -0400

    lab finished la

--- index.js
+++ index.js
@@ -8,32 +8,25 @@ const employee = {
 
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    
     return {
-        ...employee,
-        [key]: value,
-    };
-
+        ...employee, [key] : value 
+    }
 };
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-   
-    employee[key] = value;
-        return employee;
-
+        
+        employee[key] = value
+            return employee 
+    
 };
 
 function deleteFromEmployeeByKey(employee, key) {
-    
-   employee = { ...employee };
-
-    delete employee[key];
-
-    return employee;
-
-};
+    employee = { ...employee }
+        delete employee[key]
+        return employee
+    }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
+    delete employee[key]
+    return employee
 };
